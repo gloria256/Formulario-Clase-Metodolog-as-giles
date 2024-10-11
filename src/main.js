9f53803fdf960fafbe407ff0a6b566de32badcc1
rajila <rdajila@gmail.com>
2024-10-11 19:21:57 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-
+window.handlerVotar = function () {
+    let name = document.getElementById("nombre")
+    let email = document.getElementById("email")
+    let btn = document.getElementById("btnenviar")
+    btn.classList.add('hide')
+    alert('Gracias por votar')
+}
