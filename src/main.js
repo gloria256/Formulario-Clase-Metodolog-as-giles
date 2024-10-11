4b1e331f77a15be639999756cd60731db01e4b5b
Hermes Pellerano <hermes.pellerano@endurance.com>
2024-10-11 19:20:54 +0200

    feat: added header timer functionality.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-
+function countdown() {
+  var seconds = 60;
+  function tick() {
+    var counter = document.getElementById('counter');
+    seconds--;
+    counter.innerHTML = '0:' + (seconds < 10 ? '0' : '') + String(seconds);
+    if (seconds > 0) {
+      setTimeout(tick, 1000);
+    } else {
+      document.getElementById('counter').innerHTML = '';
+      if (confirm('Se acabo el tiempo!')) {
+        window.location.reload();
+      }
+    }
+  }
+  tick();
+}
+countdown();
